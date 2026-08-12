d1e5768132a2f60e21834504791cd6b4db704e84
Earthend664 <110807082+Earthend664@users.noreply.github.com>
2026-08-12 10:43:17 +1000

    Update radriar.py

--- library/radriar.py
+++ library/radriar.py
@@ -1,6 +1,13 @@
-def importFile(file_path):
+import re
+
+def importFile(file_path): # Loads a file as a list into memory, removing newlines and white-spaces
     with open(file_path, 'r') as f:
         output = []
         for line in f:
             output.append(line.strip())
     return output
+
+def extract(pattern, string): # Returns a string matching a regex
+    search = re.search(pattern, string)
+    target = search.group
+    return target
